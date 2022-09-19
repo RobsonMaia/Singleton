3c6d2112d6f06246e7e7c92a6cb711c9d77df595
RobsonMaia <robsonmaia53@gmail.com>
2022-09-19 12:26:37 -0300

    validadorComponent

--- Singleton/src/Singleton/Main.java
+++ Singleton/src/Singleton/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		GerenciadorImpresao g1 = GerenciadorImpresao.getInstance();
+		/*GerenciadorImpresao g1 = GerenciadorImpresao.getInstance();
 		GerenciadorImpresao g2 = GerenciadorImpresao.getInstance();
 		GerenciadorImpresao g3 = GerenciadorImpresao.getInstance();
 		GerenciadorImpresao g4 = GerenciadorImpresao.getInstance();
@@ -13,9 +13,17 @@ public class Main {
 		System.out.println(g2);
 		System.out.println(g3);
 		System.out.println(g4);
-		
+		*/
 		//Padrão Singleton dessa forma todos os objetos tem o mesmo endereço
 		
+	
+	ValidadorComponent validador = new ValidadorComponent();
+	
+	
+	System.out.println(validador.validaPositivo(-2));
+	
+	
 	}
+	
 
 }
---
+++ Singleton/src/Singleton/ValidadorComponent.java
@@ -0,0 +1,14 @@
+package Singleton;
+
+public class ValidadorComponent {
+	
+	public boolean validaPositivo(int valor) {
+		if (valor > 0) {
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
+
+}
